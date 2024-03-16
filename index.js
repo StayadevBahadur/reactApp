82cf6a8e12d12f5f7d40f450a33dc4ddfd7aff03
Satyadev <satyadevba18@gmail.com>
2024-03-16 09:47:31 +0530

    added all the changes in index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.lof("hi this is satya")
+//create a git repositry 
+
+// Create a folder in your desctop 
+
+// Open the folder in vs code 
+
+// initailze git in it useing cmd:git init
+// Create README.md file with # reactApp
+// the add the file using cmd: git add (fileName)
+// Then commit the changes cmd: git commit -m "added readme.md"
